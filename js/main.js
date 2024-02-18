b8567ae795225c6a3b9db21cc719fb90516c9051
LiudmylaBykova <bykova.luda@gmail.com>
2024-02-18 16:07:48 +0200

    add code

--- js/main.js
+++ js/main.js
@@ -1,21 +1,66 @@
 const burgerMenuBtn = document.querySelector(".burger-menu");
+const mobileCloseBtn = document.querySelector(".mobile-close-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileNavList = document.querySelector(".mobile-nav-list");
+const modalForm = document.querySelector(".backdrop");
+const modalFormBtn = document.querySelector(".hero-btn");
 const modalCloseBtn = document.querySelector(".modal-close-btn");
-const modal = document.querySelector(".modal-menu");
+const modalPriceFormBtn = document.querySelector(".price-btn");
+const rentForm = document.querySelector(".rent-form");
 
 burgerMenuBtn.addEventListener("click", onBurgerMenuBtn);
 function onBurgerMenuBtn() {
-  modalOpen();
+  mobileMenuOpen();
+}
+
+mobileCloseBtn.addEventListener("click", onMobileMenuCloseBtn);
+function onMobileMenuCloseBtn() {
+  mobileMenuClose();
+}
+
+mobileNavList.addEventListener("click", onMobileMenuLincClick);
+function onMobileMenuLincClick(ev) {
+  if (ev.target.nodeName !== "A") {
+    return;
+  }
+  mobileMenuClose();
+}
+// -------
+modalFormBtn.addEventListener("click", onModalFormBtn);
+function onModalFormBtn() {
+  modalFormOpen();
 }
 
 modalCloseBtn.addEventListener("click", onModalCloseBtn);
 function onModalCloseBtn() {
-  modalClose();
+  modalFormClose();
 }
 
-// ------------------------
-function modalOpen() {
-  modal.classList.add("is-open");
+modalPriceFormBtn.addEventListener("click", onModalPriceFormBtn);
+function onModalPriceFormBtn() {
+  modalFormOpen();
 }
-function modalClose() {
-  modal.classList.remove("is-open");
+// ---
+
+rentForm.addEventListener("submit", onRentFormBtn);
+function onRentFormBtn(e) {
+  e.preventDefault();
+  rentForm.reset();
+  modalFormClose();
 }
+
+//-----
+function mobileMenuOpen() {
+  mobileMenu.classList.add("is-open");
+}
+function mobileMenuClose() {
+  mobileMenu.classList.remove("is-open");
+}
+// -----
+function modalFormOpen() {
+  modalForm.classList.add("is-open");
+}
+function modalFormClose() {
+  modalForm.classList.remove("is-open");
+}
+// ================================================================
