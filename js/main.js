7f433dc3a93671212451dbeeb6444b7527b798dc
LiudmylaBykova <bykova.luda@gmail.com>
2024-02-19 15:33:50 +0200

    add code

--- js/main.js
+++ js/main.js
@@ -64,3 +64,28 @@ function modalFormClose() {
   modalForm.classList.remove("is-open");
 }
 // ================================================================
+const upBtn = document.querySelector(".up-btn");
+upBtn.addEventListener("click", onScroll);
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (window.scrollY > 200) {
+    showUpBtn();
+  } else {
+    hideUpBtn();
+  }
+}
+
+function onScroll() {
+  document.documentElement.scrollTop = 0;
+}
+function showUpBtn() {
+  upBtn.classList.remove("is-hidden");
+}
+
+function hideUpBtn() {
+  upBtn.classList.add("is-hidden");
+}
